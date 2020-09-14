f29c44d9bccbd2c6ad7e4fff2886587fd5a5f1ce
jabar <jabzahmy@gmail.com>
2020-09-14 13:00:23 +0500

    first component build with react.js

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React ,{Component} from 'react';
+import { Navbar, NavbarBrand } from 'reactstrap';
+
+import Menu from './components/Menu';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+
+        <Menu/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Menu.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import { Media } from 'reactstrap';
+
+class Menu extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            dishes: [
+                {
+                    id: 0,
+                    name: 'Uthappizza',
+                    image: 'assets/images/uthappizza.png',
+                    category: 'mains',
+                    label: 'Hot',
+                    price: '4.99',
+                    description: 'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'
+                },
+                {
+                    id: 1,
+                    name: 'Zucchipakoda',
+                    image: 'assets/images/zucchipakoda.png',
+                    category: 'appetizer',
+                    label: '',
+                    price: '1.99',
+                    description: 'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce'
+                },
+            ]
+        };
+    }
+
+    render() {
+        const menu = this.state.dishes.map((dish) => {
+            return (
+                <div key={dish.id} className='col-12 mt-5'>
+                    <Media tag='li'>
+                        <Media left middle>
+                            <Media object src={dish.image} alt={dish.image} />
+                        </Media>
+
+                        <Media body className='ml-5'>
+
+                            <Media heading>{dish.name}
+                            </Media>
+                            <p>{dish.description}</p>
+                            
+                        </Media>
+
+                    </Media>
+
+                </div>
+
+            );
+
+        })
+
+        return (
+            <div className='container' >
+
+                <div className='row'>
+
+                    {menu}
+                </div>
+
+            </div>
+
+        );
+    }
+}
+
+export default Menu;
